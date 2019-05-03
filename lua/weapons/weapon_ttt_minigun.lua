4cbc20f885c6450033bc4839b9ac2303abbed321
Xiao-Ping Huynh <huynhxiaoping@gmail.com>
2019-05-03 18:39:56 -0400

    minigun viewmodel position change

--- lua/weapons/weapon_ttt_minigun.lua
+++ lua/weapons/weapon_ttt_minigun.lua
@@ -30,7 +30,7 @@ SWEP.Kind = WEAPON_EQUIP1
 SWEP.Primary.Delay	   = 0.0775
 SWEP.Primary.Recoil	  = 0.00408
 SWEP.Primary.Automatic   = true
-SWEP.ViewModelFOV  = 40
+SWEP.ViewModelFOV  = 50
 SWEP.ViewModelFlip = false
 SWEP.CSMuzzleFlashes = true
 SWEP.Primary.ClipSize	= 200
@@ -153,4 +153,28 @@ function SWEP:PrimaryAttack(worldsnd)
 		]]
 		
 	end
+end
+
+function SWEP:GetViewModelPosition( EyePos, EyeAng )
+	local Mul = 1.0
+
+	local Offset = self.IronSightsPos
+
+	if ( self.IronSightsAng ) then
+		EyeAng = EyeAng * 1
+
+		EyeAng:RotateAroundAxis( EyeAng:Right(), 	self.IronSightsAng.x * Mul )
+		EyeAng:RotateAroundAxis( EyeAng:Up(), 		self.IronSightsAng.y * Mul )
+		EyeAng:RotateAroundAxis( EyeAng:Forward(),	self.IronSightsAng.z * Mul )
+	end
+
+	local Right 	= EyeAng:Right()
+	local Up 		= EyeAng:Up()
+	local Forward 	= EyeAng:Forward()
+
+	EyePos = EyePos + Offset.x * Right * Mul
+	EyePos = EyePos + Offset.y * Forward * Mul
+	EyePos = EyePos + Offset.z * Up * Mul
+
+	return EyePos, EyeAng
 end
